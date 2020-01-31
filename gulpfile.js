cd160d251ca2fac34da6a7ddf11d0c5d05038cfa
Mertkan <mertkanaktas@gmail.com>
2020-01-31 12:08:33 +0300

    yeni

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const { src, dest, watch, series } = require('gulp')
 const browserSync = require('browser-sync').create();
 const sass = require('gulp-sass');
+const jade = require('gulp-jade')
 
 const style = function(){
 	return src("app/scss/*.scss")
@@ -9,14 +10,20 @@ const style = function(){
         .pipe(browserSync.stream());
 }
 
+const tmp = function(){
+	return src("jade/*.jade")
+			.pipe(jade())
+			.pipe(dest("app"))
+}
+
 const serve = function(){
 	browserSync.init({
         server: "./app"
     });
 
+	watch("jade/*.jade",series(tmp))
 	watch("app/scss/*.scss", series(style));
 	watch("app/*.html").on('change', browserSync.reload);
- 	
 }
 
 exports.default = serve
